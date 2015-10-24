a5be16235b324cf57281fd1dfc34ed76b1b7f373
Gredy Prasetya <gredy.prasetya@gmail.com>
2015-10-24 21:19:32 +0700

    update db dan seeder

--- app/database/seeds/LaporanSeeder.php
+++ app/database/seeds/LaporanSeeder.php
@@ -9,6 +9,7 @@ class LaporanSeeder extends Seeder {
         $arr_kota_indonesia= array("Jakarta","Bandung","Surabaya","Medan","Palembang","Bengkulu","Semarang","Solo","Bogor","Tangerang","Banten","Denpasar");
         for($i=0;$i<100;$i++){
           $laporan = new Laporan();
+          $laporan->tipe =$faker->numberBetween($min = 0, $max = 1);
           $laporan->no_ktp_pelapor = $faker->creditCardNumber;
           $laporan->nama_pelapor = $faker->name;
           $laporan->email_pelapor = $faker->freeEmail;
@@ -17,7 +18,7 @@ class LaporanSeeder extends Seeder {
           $laporan->umur_anak = $faker->numberBetween($min = 6, $max = 15);
           $laporan->sekolah_anak = "Tidak Sekolah";
           $laporan->alamat_anak = $faker->streetAddress;
-          $laporan->kota_anak = $arr_kota_indonesia[$faker->numberBetween($min = 0, $max = 11)];;
+          $laporan->kota_anak = $arr_kota_indonesia[$faker->numberBetween($min = 0, $max = 11)];
           $laporan->keterangan = $faker->text;
           $laporan->created_by = 1;
           $laporan->save();
