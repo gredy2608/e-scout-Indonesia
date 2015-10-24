c3dc2e56f58770cda1e997fb74d313745bcbd8fd
Christopher Andrian <christandrian@gmail.com>
2015-10-24 18:29:11 +0700

    //test

--- app/controllers/Server/Database/Siswa/ReportGenerator.php
+++ app/controllers/Server/Database/Siswa/ReportGenerator.php
@@ -1,7 +1,40 @@
 <?php
 class ReportGenerator extends BaseController{
 
+	function array_sort($array, $on, $order=SORT_ASC)
+{
+    $new_array = array();
+    $sortable_array = array();
 
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+            break;
+            case SORT_DESC:
+                arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
 
 	public function getAllTopSchool(){
 		$sekolah = DB::select("SELECT * FROM sekolah");
@@ -290,7 +323,6 @@ class ReportGenerator extends BaseController{
 		$nilaiBaru = array_sort($prestasi, 'sum', SORT_DESC);
 		
 		
-	
 	}
 }
 
