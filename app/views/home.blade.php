ef7416ed7d1ee21afe1c73b5d1f0b15ebac4b624
DOMIWORKS-LAPTOP <dikomunikasikan@gmail.com>
2015-10-25 06:58:05 +0700

    Small Changes

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,7 @@
 
 <div id="content" class="col-sm-12">
 	<!-- header -->
-	<div id="header" class="row text-center" style="">
+	<div id="header" class="row text-center" style="margin-top: 60px;">
 		<img src="{{asset('assets/img/logo.png')}}" style="display:block;margin-left:auto;margin-right:auto;max-width:200px;" />
 		<h1 class="text-center">E-Scout</h1>
 		<p style="display:block;margin-left:auto;margin-right:auto;" class="col-sm-12">
@@ -14,28 +14,29 @@
 			Indonesia yang lebih baik.
 		</p>
 		<span class="clearfix"></span>
-		<a class="btn btn-info" href="{{ URL::to('/scout_now') }}" style="margin-left: auto; margin-right: auto;">
+		<a class="btn btn-material-deep-orange-A700" href="{{ URL::to('/scout_now') }}" style="margin-left: auto; margin-right: auto;">
 			Scout Now!
 		</a>
 	</div>
 	<!-- visi -->
-	<div id="visi" class="row text-center" style="">
+	<div id="visi" class="col-md-6 text-center" style="margin-top: 60px;">
 		<h1>Visi</h1>
 		<p>
 			Kami ingin menghimpun seluruh masyarakat Indonesia untuk meningkatkan mutu SDM Indonesia yang lebih baik.
 		</p>
 	</div>
 	<!-- misi -->
-	<div id="misi" class="row text-center" style="">
+	<div id="misi" class="col-md-6 text-center" style="margin-top: 60px;">
 		<h1>Misi</h1>
 		<ul style="display:inline-block;">
-			<li>misi 1</li>
-			<li>misi 2</li>
-			<li>misi 3</li>
+			<li>Collaboratively administrate empowered markets via plug-and-play networks.</li>
+			<li>Dynamically procrastinate B2C users after installed base benefits.</li>
+			<li>Dramatically visualize customer directed convergence without revolutionary ROI.</li>
 		</ul>
 	</div>
+	<span class="clearfix"></span>
 	<!-- siapa kamu -->
-	<div id="siapa_kamu" class="row text-center" style="">
+	<div id="siapa_kamu" class="row text-center" style="margin-top: 60px;">
 		<h1>Siapa kamu ?</h1>
 		<!-- scouter -->
 		<div class="col-md-4">
@@ -63,7 +64,7 @@
 		</div>
 	</div>
 	<!-- anak berprestasi -->
-	<div id="anak_berprestasi" class="row" style="">
+	<div id="anak_berprestasi" class="col-md-6" style="margin-top: 60px;">
 		<h1 style="text-align:center;">Anak Berprestasi</h1>
 		 
 		<div class="row" style="margin-top:20px;text-align:center;">
@@ -73,8 +74,8 @@
 		</div>
 	</div>
 	<!-- sekolah berprestasi -->
-	<div id="sekolah_berprestasi" class="row" style="margin-top:40px;">
-		<h1 style="text-align:center;">Sekolah - Sekolah</h1>
+	<div id="sekolah_berprestasi" class="col-md-6" style="margin-top:60px;">
+		<h1 style="text-align:center;">Sekolah</h1>
 		 
 		<div class="row" style="margin-top:20px;text-align:center;">
 			<a class="btn btn-warning" href="{{ URL::to('/schools_list') }}">
@@ -82,29 +83,30 @@
 			</a>
 		</div>
 	</div>
+	<span class="clearfix"></span>
 	<!-- hubungi kami -->
 	<div id="hubungi_kami" class="row" style="margin-top:40px;text-align:center;">
 		<h1>Hubungi Kami</h1>
-		<h4>
+		<span>
 			<img src="{{asset('assets/img/mail_icon.png')}}"
 			style="max-width:25px;" />
 			e_scout@mail.com
-		</h4>
-		<h4>
+		</span>
+		<span>
 			<img src="{{asset('assets/img/facebook_icon.png')}}"
 			style="max-width:25px;" />
 			<a href="#">Facebook</a>
-		</h4>
-		<h4>
+		</span>
+		<span>
 			<img src="{{asset('assets/img/twitter_icon.png')}}"
 			style="max-width:25px;" />
 			<a href="#">Twitter</a>
-		</h4>
-		<h4>
+		</span>
+		<span>
 			<img src="{{asset('assets/img/instagram_icon.png')}}"
 			style="max-width:25px;" />
 			<a href="#">Instagram</a>
-		</h4>
+		</span>
 	</div>
 </div>
 
